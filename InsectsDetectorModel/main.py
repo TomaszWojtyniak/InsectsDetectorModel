d121d8aa71fc3758db035890a4e557e71bf48fdd
Tomasz Wojtyniak <tomasz.w@moodup.team>
2024-04-15 19:51:20 +0200

    Add missing packages

--- InsectsDetectorModel/main.py
+++ InsectsDetectorModel/main.py
@@ -1,13 +1,4 @@
 import load_data
 
-params = {
-    "batch_size": 64,
-    "num_workers": 4,
-    "n_epochs": 10,
-    "image_size": 256,
-    "in_channels": 3,
-    "num_classes": 5
-}
-
 if __name__ == "__main__":
     loaders = load_data.create_datasets()
